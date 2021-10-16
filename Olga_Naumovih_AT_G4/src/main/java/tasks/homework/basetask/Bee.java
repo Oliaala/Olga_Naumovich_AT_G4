8049908d76307800498c08c2143568c8b13aa94c
Olga Naumovich <olga.naumovich14@gmail.com>
2021-10-16 10:45:16 +0300

    added Bee and Souse

--- Olga_Naumovih_AT_G4/src/main/java/tasks/homework/basetask/Bee.java
+++ Olga_Naumovih_AT_G4/src/main/java/tasks/homework/basetask/Bee.java
@@ -1,4 +1,32 @@
 package tasks.homework.basetask;
 
 public class Bee {
+    private String gender;
+    public long weight;
+
+    public long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(long weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Bee(int weight, String gender) {
+        this.gender = gender;
+        this.weight = weight;
+    }
+
+
+    public void printBeeDetails() {
+
+    }
 }
--- Olga_Naumovih_AT_G4/src/main/java/tasks/homework/basetask/Souse.java
+++ Olga_Naumovih_AT_G4/src/main/java/tasks/homework/basetask/Souse.java
@@ -1,4 +1,31 @@
 package tasks.homework.basetask;
 
-public class Souse {
+public class Souce {
+    private String name;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Sous(String color, String name) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public void printSouceDetails() {
+
+    }
 }
