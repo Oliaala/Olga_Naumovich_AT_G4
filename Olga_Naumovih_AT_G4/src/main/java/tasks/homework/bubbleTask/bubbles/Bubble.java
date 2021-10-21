6b148226f0982b339e60b4a22ebaf28ff8bcf69c
Olga Naumovich <olga.naumovich14@gmail.com>
2021-10-21 23:46:59 +0300

    Edded Bubble

---
+++ Olga_Naumovih_AT_G4/src/main/java/tasks/homework/bubbleTask/bubbles/Bubble.java
@@ -0,0 +1,16 @@
+package tasks.homework.bubbleTask.bubbles;
+
+public class Bubble {
+    private int volume;
+    private String nameGas;
+
+   public Bubble(int volume, String nameGas){
+       this.volume = volume;
+       this.nameGas = nameGas;
+   }
+    public void main(String[] args){
+             System.out.println("Cramp!");
+    }
+}
+
+
