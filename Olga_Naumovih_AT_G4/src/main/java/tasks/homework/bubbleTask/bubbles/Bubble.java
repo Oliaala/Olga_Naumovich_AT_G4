3bf580a605545a4695d26851c0da8defee3bca12
Olga Naumovich <olga.naumovich14@gmail.com>
2021-10-21 23:57:49 +0300

    Ended Bubble + TrMethodBubble

--- Olga_Naumovih_AT_G4/src/main/java/tasks/homework/bubbleTask/bubbles/Bubble.java
+++ Olga_Naumovih_AT_G4/src/main/java/tasks/homework/bubbleTask/bubbles/Bubble.java
@@ -4,13 +4,33 @@ public class Bubble {
     private int volume;
     private String nameGas;
 
-   public Bubble(int volume, String nameGas){
-       this.volume = volume;
-       this.nameGas = nameGas;
-   }
-    public void main(String[] args){
-             System.out.println("Cramp!");
+    public Bubble(int volume, String nameGas) {
+        this.volume = volume;
+        this.nameGas = nameGas;
+
     }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getNameGas() {
+        return nameGas;
+    }
+
+    public void setNameGas(String nameGas) {
+        this.nameGas = nameGas;
+    }
+
+    public void printBubbleDetails() {
+        System.out.println("я бабл, мой объем" + volume + " мой газовый состав " + nameGas + " я умею лопаться Cramp! ");
+
+    }
+
 }
 
 
---
+++ Olga_Naumovih_AT_G4/src/main/java/tasks/homework/bubbleTask/bubbles/TrMethodBubble.java
@@ -0,0 +1,10 @@
+package tasks.homework.bubbleTask.bubbles;
+
+public class TrMethodBubble {
+
+    public static void main(String[] args) {
+
+        Bubble bubble = new Bubble(5, "переменный");
+        new Bubble(5, "переменный").printBubbleDetails();
+    }
+    }
